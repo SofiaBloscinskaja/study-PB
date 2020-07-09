fe49cd1d748aa54f11f3c58fc7f5f01a354ea9d5
SofiaBloscinskaja <68054115+SofiaBloscinskaja@users.noreply.github.com>
2020-07-09 13:13:07 +0300

    home work less1

---
+++ task7_8.js
@@ -0,0 +1,19 @@
+//Тема 7 Чему будут равны
+let a = 1, b = 1;
+
+let c = ++a; 
+let d = b++; 
+// a=2 b=2 c=2 d=1
+let a = 2;
+let x = 1 + (a *= 2);
+//a=4 x=5
+
+//Тема 8
+//Каким будет результат этих выражений?
+5 > 4  // true
+"ананас" > "яблоко" //false
+"2" > "12" // true
+undefined == null //true
+undefined === null //false
+null == "\n0\n" //false
+null === +"\n0\n" //false
